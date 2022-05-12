dedd08ddcb1ad46546e7e455f132b3bf5289329e
dwalke22 <dwalke22@my.westga.edu>
2022-05-12 17:52:17 -0400

    New Componentes:
    Created scafolding for coach component
    Generated Navbar component

--- Endurance_Alchemy/src/app/app.module.ts
+++ Endurance_Alchemy/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoachModule } from './coach/coach.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoachModule
   ],
   providers: [],
   bootstrap: [AppComponent]
