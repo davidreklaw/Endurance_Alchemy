768e4be02b4c8f0df6eb1bb94879c43f5a254cbd
David Walker <thewalker.david@gmail.com>
2022-07-21 12:19:33 -0400

    Removed Coach component to start over

--- Endurance_Alchemy/src/app/app.module.ts
+++ Endurance_Alchemy/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoachModule } from './coach/coach.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -22,7 +21,6 @@ import { UtilityComponentsModule } from './utilities/components.module';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    CoachModule,
     BrowserAnimationsModule,
     UtilityComponentsModule
   ],
