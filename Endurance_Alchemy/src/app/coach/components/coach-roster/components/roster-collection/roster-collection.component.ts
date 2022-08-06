3621d13b6c7501fbb65d126511b84398ec873467
David Walker <thewalker.david@gmail.com>
2022-08-05 22:23:45 -0400

    Athlete Table Search:
    Completed search functionality for athlete table search

--- Endurance_Alchemy/src/app/coach/components/coach-roster/components/roster-collection/roster-collection.component.ts
+++ Endurance_Alchemy/src/app/coach/components/coach-roster/components/roster-collection/roster-collection.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface Athlete {
   name: string;
@@ -40,7 +40,12 @@ export class RosterCollectionComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
   }
 
-  applyFilter() {
-    
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
