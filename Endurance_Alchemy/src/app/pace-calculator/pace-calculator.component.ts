63467e5e228c2fd6a08afa695ae2bfb01c919e5d
David Walker <thewalker.david@gmail.com>
2023-03-01 17:21:51 -0500

    Added Calculate Method

--- Endurance_Alchemy/src/app/pace-calculator/pace-calculator.component.ts
+++ Endurance_Alchemy/src/app/pace-calculator/pace-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pace-calculator',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pace-calculator.component.scss']
 })
 export class PaceCalculatorComponent implements OnInit {
+  form: FormGroup;
+  timeHour = new FormControl({value: '', disabled: true}, [Validators.min(0)]);
+  timeMinute = new FormControl({value: '', disabled: true}, [Validators.min(0)]);
+  timeSecond = new FormControl({value: '', disabled: true}, [Validators.min(0)]);
+  distance = new FormControl({value: '', disabled: true}, [Validators.min(0)]);
+  distanceType = new FormControl({value: '', disabled: true});
+  paceHour = new FormControl({value: '', disabled: true}, [Validators.min(0)]);
+  paceMinute = new FormControl({value: '', disabled: true}, [Validators.min(0)]);
+  paceSecond = new FormControl({value: '', disabled: true}, [Validators.min(0)]);
+  paceType = new FormControl({value: '', disabled: true});
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.form = this.initForm();
   }
 
+  initForm() {
+    const form = {
+      timeHour: this.timeHour,
+      timeMinute: this.timeMinute,
+      timeSecond: this.timeSecond,
+      distance: this.distance,
+      distanceType: this.distanceType,
+      paceHour: this.paceHour,
+      paceMinute: this.paceMinute,
+      paceSecond: this.paceSecond,
+      paceType: this.paceType
+    }
+    return this.formBuilder.group(form);
+  }
+
+  calculate() {
+
+  }
 }
